aa433e4389b53de4281e79119aba741ffdc6be77
Daniel <100878892+wantinglittle@users.noreply.github.com>
2022-05-07 15:20:37 -0600

    update

---
+++ countries-api/precache-manifest.31c52e3ed9866629a44de3e2f723c62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3e2b0c37eb5fb021cf6",
+    "url": "/static/css/main.b8f1ec15.chunk.css"
+  },
+  {
+    "revision": "b3e2b0c37eb5fb021cf6",
+    "url": "/static/js/main.b3e2b0c3.chunk.js"
+  },
+  {
+    "revision": "420c2a30c024cbe4d53c",
+    "url": "/static/js/1.420c2a30.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9fc8685a906093af907489a4f3a9d5e8",
+    "url": "/index.html"
+  }
+];
